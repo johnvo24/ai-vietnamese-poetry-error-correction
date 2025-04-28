5d2726be1f56f104d511c759a47350779704c62a
John Vo <vochidung243@gmail.com>
2025-04-28 18:52:56 +0700

    John updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.dataset_handler import DatasetHandler
 from src.models import VpecGPT2
 from src import helper
+from Jvai import GDrive
 
 dataset_handler = DatasetHandler()
 
@@ -14,17 +15,20 @@ def train_vpec_gpt2():
     val_loader=val_loader
   )
 
-def generate_vpec_gpt2():
+def generate_vpec_gpt2(g_drive=False):
   vpec_gpt2 = VpecGPT2()
-  checkpoint = helper.load_checkpoint(
-    model_dir=vpec_gpt2.model_name,
-    model=vpec_gpt2.model,
-    optimizer=vpec_gpt2.optimizer,
-    is_the_best=True
-  )
+  if g_drive:
+    checkpoint = GDrive().load_model_from_drive('best_checkpoint.tar', vpec_gpt2.model_name)
+  else:
+    checkpoint = helper.load_checkpoint(
+      model_dir=vpec_gpt2.model_name,
+      model=vpec_gpt2.model,
+      optimizer=vpec_gpt2.optimizer,
+      is_the_best=True
+    )
   vpec_gpt2.model = checkpoint['model']
   vpec_gpt2.optimizer = checkpoint['optimizer']
   vpec_gpt2.__generate__("<sop> Con cò mà đi ăn đêm\nĐậu phải cành đào lộn cổ xuống ao <eop> <reasoning_memory> Bất kể mọi lúc đều có thể có rủi ro. <eois>", 256)
 
-train_vpec_gpt2()
-# generate_vpec_gpt2()
+# train_vpec_gpt2()
+generate_vpec_gpt2()
